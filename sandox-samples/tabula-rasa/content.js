7f186ce4ad41dd17f17c8c3c25143cf32d7e628c
unknown <surana.h@gmail.com>
2023-09-07 12:44:38 +0530

    fix: get the list working for YT playlists

--- sandox-samples/tabula-rasa/content.js
+++ sandox-samples/tabula-rasa/content.js
@@ -12,10 +12,35 @@ function getPageTitle() {
 }
 
 // Listening for a message from the popup
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       if(request.message === "clicked_browser_action") {
+//         sendResponse({title: getPageTitle()});
+//     }
+//     }
+//   );
+
+  function getAllVideoTitles() {
+    // Find all video title elements in the playlist
+    let videoElements = document.querySelectorAll('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer');
+    
+    console.log(videoElements);
+
+    // Extract and return all video titles
+    let titles = [];
+    videoElements.forEach(element => {
+        titles.push(element.innerText.trim());
+    });
+
+    return titles;
+}
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if(request.message === "clicked_browser_action") {
-        sendResponse({title: getPageTitle()});
+  function(request, sender, sendResponse) {
+    if(request.message === "clicked_browser_action") {
+      sendResponse({titles: getAllVideoTitles()});
     }
-    }
-  );
+  }
+);
+
+
